b76c36214a33b4acdab0368d6164c4c6c6b7070b
chirsz-ever <chirsz@foxmail.com>
2025-01-14 11:44:55 +0800

    Add basic content and 00_c23_embed

---
+++ 00_c23_embed/main_c32_embed.c
@@ -0,0 +1,24 @@
+#include <stdint.h>
+#include <stdio.h>
+
+const char example_string[] = {
+#embed "../example_string.txt" suffix(, '\0')
+};
+
+const uint8_t example_binary[] = {
+#embed "../example_binary.png"
+};
+
+void dump(const uint8_t arr[], size_t size)
+{
+    for (size_t i = 0; i != size; ++i)
+        printf("%02X ", arr[i]);
+    puts(" ...");
+}
+
+int main()
+{
+    printf("example_string[]: %zu bytes, content: %.*s ...\n", sizeof example_string, 30, example_string);
+    printf("example_binary[]: %zu bytes, content: ", sizeof example_binary);
+    dump((const uint8_t *)example_binary, 10);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+
+The png file <example_binary.png> is from: https://www.iconarchive.com/show/papirus-apps-icons-by-papirus-team/google-photos-icon.html
